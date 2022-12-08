24a5f44892b0dfd2bbc3dbbffd76979cf0a09f1d
gopal-sakshi <gopal612@gmail.com>
2022-12-08 16:01:26 +0530

    get ip

--- routes/quotes/quoteRouter.js
+++ routes/quotes/quoteRouter.js
@@ -3,12 +3,18 @@ const path = require('path');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log(req.socket.remoteAddress);          // ::1 ====> same as '127.0.0.1', which is the IPv4 version of the loopback address; ::1 is IPv6
-    console.log(req.ip);
     res.sendFile('index.html', {root: path.join(__dirname, '../../resources')});
     // we told Express to serve an index.html file 
 });
 
+router.get('/getIp', (req, res) => {
+    var responseObject = {
+        remoteAddr: req.socket.remoteAddress,
+        ip: req.ip
+    }    
+    console.log(req.socket.remoteAddress);          // ::1 ====> same as '127.0.0.1', which is the IPv4 version of the loopback address; ::1 is IPv6
+    res.status(200).json(responseObject);
+})
 // for some reason, it isnt working... look into it later...
 router.post('/addQuote', (req, res) => {
     console.log('Hellooooooooooooooooo!');
